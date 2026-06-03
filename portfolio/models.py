3daa29cd3148d9c1fa2bcb3f560fd348155f7579
varunnatesh <varunnatesh10@gmail.com>
2026-06-03 18:13:00 +0530

    Revert to working version before GitHub links changes

--- portfolio/models.py
+++ portfolio/models.py
@@ -153,7 +153,7 @@ class SiteConfiguration(models.Model):
     
     # About Section
     about_title = models.CharField(max_length=200, default="About Me")
-    about_description = models.TextField(blank=True, default="")
+    about_description = models.TextField()
     profile_image = models.ImageField(upload_to='profile/', blank=True, null=True)
     
     # Contact Info
