c03ba0766b11f59d58468b64d241cd158bdc5c4d
varunnatesh <varunnatesh10@gmail.com>
2026-06-03 18:30:55 +0530

    Simplify admin password

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ python manage.py migrate
 python manage.py load_initial_data
 
 # Create superuser automatically if it doesn't exist
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'varunnatesh10@gmail.com', '@10varun')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'varunnatesh10@gmail.com', 'admin123varun')" | python manage.py shell
 
